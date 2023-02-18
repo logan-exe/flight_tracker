215b57bb6bf6ca9eb8a19ca545d46ec4f32a4d40
Arun Loganathan <arunloganathan@glaroam2-146-93.wireless.gla.ac.uk>
2023-02-18 19:28:45 +0000

    first commit

---
+++ public/env.js
@@ -0,0 +1,3 @@
+window.env = {
+  "API_KEY": "4a213b3e6amsh66a5d11976f3175p1f03c5jsnbf563af37a20"
+};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import env from "react-dotenv";
 function App() {
+  const [flightStatus, setFlightStatus] = useState();
+  const [flighId, setFlightId] = useState();
+  const [loading, setLoading] = useState();
+  console.log(env.API_KEY, "this is api key ");
+
+  const getFlightStatus = async () => {
+    setLoading(true);
+    const options = {
+      method: "GET",
+      url: "https://flight-radar1.p.rapidapi.com/flights/search",
+      params: { query: flighId, limit: "25" },
+      headers: {
+        "X-RapidAPI-Key": env.API_KEY,
+        "X-RapidAPI-Host": "flight-radar1.p.rapidapi.com",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setFlightStatus(response.data);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        console.error(error);
+        setLoading(false);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        onChange={(e) => setFlightId(e.target.value)}
+        style={{ width: "50%", marginTop: "40px", padding: "12px" }}
+        placeholder="Enter flight Number"
+      />
+      <button onClick={() => getFlightStatus()} style={{ padding: "12px" }}>
+        {" "}
+        {loading ? "loading..." : "Search Flight"}
+      </button>
+      <div className="table">
+        {flightStatus ? (
+          <table>
+            <thead>
+              <th>Flight id</th>
+              <th>Flight Label</th>
+              <th>lon</th>
+              <th>lat</th>
+              <th>Route</th>
+              <th>From</th>
+              <th>To</th>
+              <th>Type</th>
+            </thead>
+            <tbody>
+              {flightStatus.results.map((flight, index) => {
+                return (
+                  <tr>
+                    <td>{flight.id}</td>
+                    <td>{flight.label}</td>
+                    <td>{flight.detail.lon}</td>
+                    <td>{flight.detail.lat}</td>
+                    <td>{flight.detail.route}</td>
+                    <td>{flight.type}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
